17e40809715254b4301d24440a61976ad83b5308
xd vape <55017749+xd-vape@users.noreply.github.com>
2020-09-04 11:23:02 +0200

    Add files via upload

---
+++ teamspeak_installerV2.sh
@@ -0,0 +1,191 @@
+#!/bin/bash
+# Teamspeak³ Server installer by xd vape
+
+MACHINE=$(uname -m)
+Instversion="1.0"
+
+# Functions
+
+function greenMessage() {
+  echo -e "\\033[32;1m${*}\\033[0m"
+}
+
+function magentaMessage() {
+  echo -e "\\033[35;1m${*}\\033[0m"
+}
+
+function cyanMessage() {
+  echo -e "\\033[36;1m${*}\\033[0m"
+}
+
+function redMessage() {
+  echo -e "\\033[31;1m${*}\\033[0m"
+}
+
+function yellowMessage() {
+  echo -e "\\033[33;1m${*}\\033[0m"
+}
+
+function whiteMessage() {
+  echo -e "\\033[39;1m${*}\\033[0m"
+}
+
+function errorQuit() {
+  errorExit 'Exit now!'
+}
+
+function errorExit() {
+  redMessage "${@}"
+  exit 1
+}
+
+function makeDir() {
+  if [ -n "$1" ] && [ ! -d "$1" ]; then
+    mkdir -p "$1"
+  fi
+}
+
+# clear
+# greenMessage "[..........................] (0%)"
+# apt-get install sudo -y >/dev/null
+# clear
+# greenMessage "[#.........................] (10%)"
+# sudo apt-get install curl -y >/dev/null
+# clear
+# greenMessage "[##........................] (15%)" 
+# sudo apt-get install wget -y >/dev/null
+# clear
+# greenMessage "[###.......................] (27%)"
+# sudo apt-get install nano -y >/dev/null
+# clear
+# greenMessage "[#####.....................] (36%)"
+# sudo apt-get install screen -y >/dev/null
+# clear
+# greenMessage "[######....................] (45%)"
+# sudo apt-get install htop -y >/dev/null
+# clear
+# greenMessage "[#############.............] (59%)"
+# sudo apt-get install git -y >/dev/null
+# clear
+# greenMessage "[#################.........] (68%)"
+# sudo apt-get install tar -y >/dev/null
+# clear
+# greenMessage "[####################......] (76%)"
+# sudo apt-get install bzip -y >/dev/null
+# clear
+# greenMessage "[#######################...] (86%)" 
+# sudo apt-get install unzip -y >/dev/null
+# clear
+# greenMessage "[#########################.] (96%)" 
+# sudo apt-get update -y >/dev/null
+# clear
+# whiteMessage "[##########################] (100%)"
+# sudo apt-get upgrade -y >/dev/null
+clear
+
+# Check if the script was run as root user. Otherwise exit the script
+if [ "$(id -u)" != "0" ]; then
+    errorExit "Change to root account required!"
+fi
+
+if [ "$MACHINE" == "x86_64" ]; then
+  ARCH="amd64"
+else
+  errorExit "$MACHINE is not supported"!
+fi
+
+sleep 1
+echo "┌──────────────────────────────────────────────┐"
+echo "│                                              │"
+echo "│         Teamspeak³ Server Installer          │"
+echo "│               Installer v$Instversion                 │"
+echo "│              Script by xd vape               │"
+echo "│                                              │"
+echo "├──────────────────────────────────────────────┤"
+echo "│                                              │"
+echo "│  1. TS3 Server Installieren                  │"
+echo "│  2. TS3 Server Entfernen                     │"
+echo "│  3. TS3 Server Starten                       │"
+echo "│  4. Installer Quit                           │"
+echo "│                                              │"
+echo "└──────────────────────────────────────────────┘"
+read -p "Wähle eine Option aus: " opt
+
+
+# TS Install
+if [ "$opt" = "1" ]; then
+  clear
+  sleep 1
+  echo "┌──────────────────────────────────────────────┐"
+  echo "│                                              │"
+  echo "│         Teamspeak³ Server Installer          │"
+  echo "│               Installer v1.0                 │"
+  echo "│              Script by xd vape               │"
+  echo "│                                              │"
+  echo "└──────────────────────────────────────────────┘"
+  echo "Server automatisch installieren oder selber entscheiden?" 
+  select installdir in "Automatisch" "Eigener Ordner" "Quit"
+  do
+  if [ "$installdir" == "Automatisch" ]
+  VERSION=3.12.1
+  SUUSER=ts3server_amd64
+  LOCATION=/home/ts3server_amd64
+  then
+  clear
+    yellowMessage "Teamspeak Server wird heruntergeladen..."
+    makeDir $LOCATION
+    cd $LOCATION || return
+    DOWNLOAD_URL_VERSION="https://files.teamspeak-services.com/releases/server/$VERSION/teamspeak3-server_linux_$ARCH-$VERSION.tar.bz2"
+    STATUS=$(wget --server-response -L $DOWNLOAD_URL_VERSION 2>&1 | awk '/^  HTTP/{print $2}')
+    if [ "$STATUS" == "200" ]; then
+        DOWNLOAD_URL=$DOWNLOAD_URL_VERSION
+    fi
+    tar -xvf teamspeak3-server_linux_$ARCH-$VERSION.tar.bz2 >/dev/null
+    rm teamspeak3-server_linux_$ARCH-$VERSION.tar.bz2
+    cd teamspeak3-server_linux_$ARCH || return
+    mv * ../ 
+    cd ..
+    rmdir teamspeak3-server_linux_$ARCH/
+    greenMessage "Teamspeak Server erfolgreich installiert!"
+      echo "Soll der Server gestartet werden?" 
+      select start_server in "Ja" "Nein"
+      do
+      if [ "$start_server" == "Ja" ]
+      then
+        echo "Test"
+      fi
+      if [ "$start_server" == "Nein" ]
+      then
+        errorExit 'Installer wird verlassen!'
+        exit 0
+      fi
+      done
+    fi
+
+    if [ "$installdir" == "Eigener Ordner" ]
+    then
+        read -p "Wo soll der Server installiert werden? :" installowndir
+        mkdir $installowndir
+        cd $installowndir
+    fi
+
+    if [ "$installdir" == "Quit" ]
+    then
+        errorExit 'Installer wird verlassen!'
+        exit 0
+    fi
+  done
+fi
+
+if [ "$opt" = "2" ]; then
+    echo "Test"
+fi
+
+if [ "$opt" = "3" ]; then
+ echo "Test3"
+fi
+
+if [ "$opt" = "4" ]; then
+ echo "Ciao"
+ exit 0
+fi
